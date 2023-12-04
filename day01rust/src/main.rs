dd9e0ca7801c4f84130fe0581be39c1a182d1b6e
Christopher Wilson <cgw@thewilsonadventure.com>
2023-12-03 22:29:04 -0600

    Day 1 p1 working in rust.

--- day01rust/src/main.rs
+++ day01rust/src/main.rs
@@ -15,18 +15,24 @@ fn main() -> std::io::Result<()> {
     let filename = &args[1];
     let f = File::open(filename)?;
 
-    let mut reader = BufReader::new(f);
-    let mut line = String::new();
+    let reader = BufReader::new(f);
+    //let mut line = String::new();
     let mut sum = 0u64;
-    let len = reader.read_line(&mut line)?;
-    println!("First line is {len} bytes: {line}\n", len=len, line=line);
-    let first_int = find_first_int(&line);
-    let last_int = find_last_int(&line);
-    println!("\tFirst int is {:?} \n", first_int);
-    println!("\tLast int is {:?} \n", last_int);
-    let number = first_int.unwrap() * 10 + last_int.unwrap();  
-    println!("\tNumber is {:?} \n", number);
-    sum = sum + number as u64;
+    //let len = reader.read_line(&mut line)?;
+    for line_read in reader.lines() {
+        let line = line_read?;
+        let len = line.len();
+        println!("First line is {len} bytes: {line}\n", len=len, line=line);
+        
+        let first_int = find_first_int(&line);
+        let last_int = find_last_int(&line);
+        println!("\tFirst int is {:?} \n", first_int);
+        println!("\tLast int is {:?} \n", last_int);
+        let number = first_int.unwrap() * 10 + last_int.unwrap();  
+        println!("\tNumber is {:?} \n", number);
+        sum = sum + number as u64;
+        //line_read.unwrap().clear();
+    }
 
     println!("\tSum: {}", sum);
     
