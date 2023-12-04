472bd953d1394b0ccf10b6fdc7d41dcabcb7d71c
Christopher Wilson <cgw@thewilsonadventure.com>
2023-12-03 23:31:57 -0600

    Revamp Day 1 for rust. Working p2

--- day01rust/src/main.rs
+++ day01rust/src/main.rs
@@ -10,31 +10,40 @@ fn find_last_int(input: &str) -> Option<u32> {
     input.chars().rfind(|a| a.is_digit(10)).and_then(|a| a.to_digit(10))
 }
 
+fn convert_line(input: &str) {
+    let words = vec!["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut loc = Vec::new();
+    for w in words {
+        loc.push ( input.find(w) );
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let f = File::open(filename)?;
 
     let reader = BufReader::new(f);
-    //let mut line = String::new();
     let mut sum = 0u64;
-    //let len = reader.read_line(&mut line)?;
+    let mut sump2 = 0u64;
     for line_read in reader.lines() {
         let line = line_read?;
-        let len = line.len();
-        println!("First line is {len} bytes: {line}\n", len=len, line=line);
-        
+        println!("First line is {}", line=line);
         let first_int = find_first_int(&line);
         let last_int = find_last_int(&line);
-        println!("\tFirst int is {:?} \n", first_int);
-        println!("\tLast int is {:?} \n", last_int);
+        println!("\tFirst int is {:?}", first_int);
+        println!("\tLast int is {:?}", last_int);
         let number = first_int.unwrap() * 10 + last_int.unwrap();  
-        println!("\tNumber is {:?} \n", number);
+        println!("\tNumber is {:?}", number);
         sum = sum + number as u64;
         //line_read.unwrap().clear();
+
+        let p2line = convert_line(&line);
+        println!("\tp2line: {:?}", p2line);
     }
 
-    println!("\tSum: {}", sum);
+    println!("Sum: {}", sum);
+    println!("Sum P2: {}", sump2);
     
     Ok(())
 }
