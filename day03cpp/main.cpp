9815c0d9b2cb2e2d1e3bd0d14ab2e60498d1ab16
Christopher Wilson <cgw@thewilsonadventure.com>
2023-12-09 21:15:22 -0600

    Cleaning up day3.

--- day03cpp/main.cpp
+++ day03cpp/main.cpp
@@ -47,24 +47,6 @@ std::vector<symbol_t> symbols;
 std::vector<number_t> numbers;
 std::map<symbol_t,vector<number_t>> gears;
 
-void parse_line2(string line, int row){
-    std::replace(line.begin(), line.end(),'.',' ');
-    stringstream ss(line);
-    cout <<line <<endl;
-    string s;
-    char c;
-    int col = 0;
-    while (ss){
-        ss.get(c);
-        if (ss){
-            ++col;
-            if (isdigit(c))
-                s.push_back(c);
-        }
-    }
-    cout <<" s: " <<s << " gcount: "<< ss.gcount() <<endl;
-}
-
 void parse_line (string line, int row){
     auto start_it = std::find_if(line.begin(),line.end(),[](const char c) { return c != '.'; });
     auto end_it = std::find_if(start_it+1,line.end(),[](const char c) { return !isdigit(c); });
@@ -74,7 +56,7 @@ void parse_line (string line, int row){
         auto substr_count = stop_pos - start_pos;
         substr_count = std::max(substr_count,1UL);
         auto sub = line.substr(start_pos, substr_count);
-        cout <<"PL: " <<sub <<endl;
+        //cout <<"PL: " <<sub <<endl;
         if (isdigit(sub[0])){
             number_t n;
             n.num = stoi(sub);
@@ -137,54 +119,50 @@ int main(int argc, char* argv[]){
     string line;
     uint64_t line_count = 0;
     uint64_t game_number;
-    uint64_t sum = 0;
-    uint64_t power_sum = 0;
     while(getline(in,line)){
         if (line.empty()){
             ++line_count;
             continue;
         }
-        cout <<"========\n" <<line <<endl;
+        //cout <<"========\n" <<line <<endl;
         parse_line(line, line_count);
         ++line_count;
     }
-    cout <<"Symbols: " <<endl;
-    for (const auto& s : symbols){
-        cout <<"\t" <<s <<endl;
-    }
-    cout <<"Numbers: " <<endl;
-    for (const auto& n : numbers){
-        cout <<"\t" <<n <<endl;
-    }
-    cout <<"Sum of game ids is: " <<sum<<endl;
-    cout <<"Sum of power is: " <<power_sum <<endl;
+    //cout <<"Symbols: " <<endl;
+    //for (const auto& s : symbols){
+    //    cout <<"\t" <<s <<endl;
+    //}
+    //cout <<"Numbers: " <<endl;
+    //for (const auto& n : numbers){
+    //    cout <<"\t" <<n <<endl;
+    //}
 
     auto s2 = find_symbols_near_row(1);
-    cout <<"find_symbols_near_row(2):\n";
-    for (const auto& s : s2){
-        cout <<"\t" <<s <<endl;
-    }
+    //cout <<"find_symbols_near_row(2):\n";
+    //for (const auto& s : s2){
+    //    cout <<"\t" <<s <<endl;
+    //}
     auto s3= find_symbols_near_cols(s2, 0, 2);
-    cout <<"find_symbols_near_cols(0,2):\n";
-    for (const auto& s : s3){
-        cout <<"\t" <<s <<endl;
-    }
+    //cout <<"find_symbols_near_cols(0,2):\n";
+    //for (const auto& s : s3){
+    //    cout <<"\t" <<s <<endl;
+    //}
 
     uint64_t pnsum = 0;
     for (auto& n : numbers){
-        cout <<"Running number: " <<n <<endl;
+        //cout <<"Running number: " <<n <<endl;
         int row = n.start.row;
         auto s2 = find_symbols_near_row(row);
-        cout <<"find_symbols_near_row(" <<row<<"):\n";
-        for (const auto& s : s2){
-            cout <<"\t" <<s <<endl;
-        }
+        //cout <<"find_symbols_near_row(" <<row<<"):\n";
+        //for (const auto& s : s2){
+        //    cout <<"\t" <<s <<endl;
+        //}
         int sc = n.start.col;
         int ec = n.end.col;
         auto s3= find_symbols_near_cols(s2, sc, ec);
-        cout <<"find_symbols_near_cols("<<sc <<", " <<ec <<"):\n";
+        //cout <<"find_symbols_near_cols("<<sc <<", " <<ec <<"):\n";
         for (const auto& s : s3){
-            cout <<"\t" <<s <<endl;
+            //cout <<"\t" <<s <<endl;
             if (s.sym == '*'){
                 gears[s].push_back(n);
             }
@@ -194,22 +172,22 @@ int main(int argc, char* argv[]){
             pnsum += n.num;
         }
     }
-    cout <<"Numbers: " <<endl;
-    for (const auto& n : numbers){
-        cout <<"\t" <<n <<endl;
-    }
+    //cout <<"Numbers: " <<endl;
+    //for (const auto& n : numbers){
+    //    cout <<"\t" <<n <<endl;
+    //}
     cout <<"PN Sum: " <<pnsum <<endl;
 
-    cout <<" Gears: " <<endl;
+    //cout <<" Gears: " <<endl;
     for (auto g = gears.begin(); g != gears.end(); ){
-        cout <<"\t" <<g->first << ": " <<endl;
+        //cout <<"\t" <<g->first << ": " <<endl;
         auto gear_count = g->second.size();
-        for (const auto& s : g->second){
-            cout <<"\t\t" <<s <<endl;
-        }
+        //for (const auto& s : g->second){
+        //    cout <<"\t\t" <<s <<endl;
+        //}
         if (gear_count == 1){
             g = gears.erase(g);
-            cout << "\t\t not a gear."<<endl;
+            //cout << "\t\t not a gear."<<endl;
         } else {
             ++g;
         }
@@ -222,8 +200,8 @@ int main(int argc, char* argv[]){
             gear_product *= n.num;
         }
         gear_sum += gear_product;
-        cout <<"\tGear product: " <<gear_product <<endl;
-        cout <<"\tGear sum: " <<gear_sum <<endl;
+        //cout <<"\tGear product: " <<gear_product <<endl;
+        //cout <<"\tGear sum: " <<gear_sum <<endl;
     }
 
     cout <<"Gear sum: " <<gear_sum <<endl;
